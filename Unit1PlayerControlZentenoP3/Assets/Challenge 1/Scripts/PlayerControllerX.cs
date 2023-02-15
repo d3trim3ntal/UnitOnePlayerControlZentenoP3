3b838aaaf5f81c9c8a0b5fd191cb7c950cf5e744
d3trim3ntal <obsidianblackrose@gmail.com>
2023-02-15 11:41:13 -0800

    Everything except the bonus is done
    almost

--- Unit1PlayerControlZentenoP3/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Unit1PlayerControlZentenoP3/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
-    public float verticalInput;
+    private float speed = 20f;
+    private float rotationSpeed = 45.0f;
+    private float verticalInput;
+    private float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,12 @@ public class PlayerControllerX : MonoBehaviour
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, rotationSpeed * horizontalInput * Time.deltaTime);
     }
 }
