93129bc2f7c973689628c81df6925b142c6d7efd
d3trim3ntal <obsidianblackrose@gmail.com>
2023-02-17 11:43:17 -0800

    Done with bonus features
    done only need lab

--- Unit1PlayerControlZentenoP3/Assets/Scripts/PlayerController.cs
+++ Unit1PlayerControlZentenoP3/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
     private float speed = 20;
     private float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
+    public string inputID;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +21,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+        
         // Move the vehicle forward
-
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
